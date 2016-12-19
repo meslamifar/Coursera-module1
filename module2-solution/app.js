bc22e585005b2cdf21842b53c818c9d87b648bb0
Mahshad Eslamifar <mahshad.sing@gmail.com>
2016-12-18 22:16:32 -0500

    module2-solution

---
+++ module2-solution/app.js
@@ -0,0 +1,81 @@
+(function () {
+    
+'use strict';  
+
+angular.module("ShoppingListCheckOff",[])
+        .controller("AlreadyBoughtController", AlreadyBoughtController)
+        .controller("ToBuyController", ToBuyController)
+        .service("ShoppingListCheckOff", ShoppingListCheckOffService);
+ 
+
+function ShoppingListCheckOffService(){
+    
+    var service = this;
+    
+    service.toBuyList = [
+       { name: "cookies", quantity: 10 },
+       { name: "cookies", quantity: 10 },
+       { name: "cookies", quantity: 10 },
+       { name: "cookies", quantity: 10 },
+       { name: "cookies", quantity: 10 }                           
+    ];
+    
+    service.alreadyBoughtList = [];
+    
+    service.messages = { isAlreadyBoughtListEmpty : true , isToBuyListEmpty : false };
+    
+    service.getToBuyList = function(){
+        return service.toBuyList;
+    }
+    
+    service.getAlreadyBoughtList = function(){
+        return service.alreadyBoughtList;
+    }
+    
+    service.updateShoppingList = function(index){
+        var boughtItem = service.toBuyList[index];
+        service.toBuyList.splice(index,1);
+        service.alreadyBoughtList.push(boughtItem);
+        
+        service.updateMessages();
+    }
+    
+    service.updateMessages = function(){
+        service.messages.isAlreadyBoughtListEmpty = service.alreadyBoughtList.length == 0;
+        service.messages.isToBuyListEmpty = service.toBuyList.length == 0;
+    }
+    
+    service.getMessages = function(){        
+        return service.messages;
+    }
+    
+        
+}  
+
+ToBuyController.$inject = ['ShoppingListCheckOff', '$scope'];
+function ToBuyController(ShoppingListCheckOff, $scope){
+    
+    var toBuy = this;
+    
+    console.log($scope);
+    toBuy.toBuyList = ShoppingListCheckOff.getToBuyList();
+    
+    toBuy.updateShoppingList = function(index){
+        ShoppingListCheckOff.updateShoppingList(index);
+    }
+    
+    toBuy.messages = ShoppingListCheckOff.getMessages();
+}
+    
+AlreadyBoughtController.$inject = ['ShoppingListCheckOff'];    
+function AlreadyBoughtController(ShoppingListCheckOff){
+    
+    var alreadyBought = this;
+    
+    alreadyBought.alreadyBoughtList = ShoppingListCheckOff.getAlreadyBoughtList(); 
+    
+    alreadyBought.messages = ShoppingListCheckOff.getMessages();
+    
+}  
+ })();
+    
